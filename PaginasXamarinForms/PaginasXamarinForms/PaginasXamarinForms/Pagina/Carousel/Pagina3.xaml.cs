6d397ed2daf75a91fac45432a7fd95af258d7e0b
EduardoReisDev <52722526+EduardoReisDev@users.noreply.github.com>
2019-07-18 20:52:06 -0300

    Segundo projeto

---
+++ PaginasXamarinForms/PaginasXamarinForms/PaginasXamarinForms/Pagina/Carousel/Pagina3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PaginasXamarinForms.Pagina.Carousel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina3 : ContentPage
+    {
+        public Pagina3()
+        {
+            InitializeComponent();
+        }
+    }
+}
